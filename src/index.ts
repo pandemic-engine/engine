3e502f4dcea7086073dea289e6cac1a7a3e2b2a6
Jarret Byrne <1619065+stratedge@users.noreply.github.com>
2020-03-23 19:58:35 -0400

    Add new index.ts to compile library

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// Classes
+export { Component } from './Components/Component'
+export { Engine } from './Engine'
+export { Entity } from './Entity'
+export { MovementSystem } from './Systems/MovementSystem'
+export { System } from './Systems/System'
+export { TransformComponent } from './Components/TransformComponent'
+
+// Types
+export { ComponentConstructor } from './types/ComponentConstructor'
